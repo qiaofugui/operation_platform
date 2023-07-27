c8991255d3b14a97e4bcbfecc2b01d9f578a93e9
qiaoronggui <903111082@qq.com>
2023-07-27 20:27:22 +0800

    test: test

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import '@/styles/index.scss'
+
 import 'virtual:svg-icons-register'
 
 import globalComponent from './components/index'
