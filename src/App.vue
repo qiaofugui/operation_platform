b28d7646e4f41c08e704a4b4f510c483e5c5623c
qiaoronggui <903111082@qq.com>
2023-07-27 18:01:00 +0800

    build: 配置 自动引入element-plus 组件和 icon 图标

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <script setup lang="ts"></script>
 
-<template>App</template>
+<template>
+  <div>App</div>
+  <el-button type="primary">Primary</el-button>
+  <el-icon>
+    <IEpUserFilled />
+  </el-icon>
+</template>
 
 <style scoped></style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,36 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
+import Icons from 'unplugin-icons/vite'
+import IconsResolver from 'unplugin-icons/resolver'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+
+    AutoImport({
+      resolvers: [
+        ElementPlusResolver(),
+        IconsResolver({
+          prefix: 'Icon',
+        }),
+      ],
+    }),
+    Components({
+      resolvers: [
+        ElementPlusResolver(),
+        IconsResolver({
+          enabledCollections: ['ep'],
+        }),
+      ],
+    }),
+    Icons({
+      autoInstall: true,
+    }),
+  ],
 })
