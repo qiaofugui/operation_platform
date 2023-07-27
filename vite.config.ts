a8c1da9213c7011934b8cbfebcd0d8caf529d744
qiaoronggui <903111082@qq.com>
2023-07-27 18:06:09 +0800

    build: 配置 src 别名 @

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import path from 'path'
+
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
@@ -33,4 +35,9 @@ export default defineConfig({
       autoInstall: true,
     }),
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve('./src'), // 相对路径别名配置，使用 @ 代替 src
+    },
+  },
 })
