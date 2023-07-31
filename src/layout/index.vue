83d115a6a928f4f45b2446054ad2009886920a16
qiaoronggui <903111082@qq.com>
2023-07-31 14:52:32 +0800

    feat: 左侧菜单静态搭建

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -7,6 +7,21 @@ import Logo from './logo/index.vue'
     <!-- 左侧菜单 -->
     <div class="layout_slider">
       <Logo />
+      <el-scrollbar class="scrollbar">
+        <el-menu background-color="$base-menu-background" text-color="#fff">
+          <el-menu-item index="1">首页</el-menu-item>
+          <el-menu-item index="2">数据大屏</el-menu-item>
+
+          <el-sub-menu index="3">
+            <template #title>
+              权限管理
+            </template>
+            <el-menu-item index="3-1">用户管理</el-menu-item>
+            <el-menu-item index="3-2">角色管理</el-menu-item>
+            <el-menu-item index="3-3">菜单管理</el-menu-item>
+          </el-sub-menu>
+        </el-menu>
+      </el-scrollbar>
     </div>
     <!-- 顶部导航 -->
     <div class="layout_tabbar">456</div>
@@ -27,6 +42,11 @@ import Logo from './logo/index.vue'
     width: $base-menu-width;
     height: 100vh;
     background-color: $base-menu-background;
+
+    .scrollbar {
+      width: 100%;
+      height: calc(100vh - $base-tabbar-height);
+    }
   }
 
   .layout_tabbar {
