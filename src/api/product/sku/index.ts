41904d1d739743dcbc12eeca8d6912a4a754a3e7
qiaoronggui <903111082@qq.com>
2023-08-05 16:43:51 +0800

    feat: SKU 页面展示已有SKU

--- src/api/product/sku/index.ts
+++ src/api/product/sku/index.ts
@@ -1,1 +1,16 @@
 import request from '@/utils/request'
+
+import type { SkuResponseData } from './type'
+
+enum API {
+  // 获取已有商品数据-sku列表
+  SKU_URL = '/admin/product/list/'
+}
+
+// 获取已有商品数据-sku列表
+export function getSkuListAPI(page: number, limit: number) {
+  return request<any, SkuResponseData>({
+    url: `${API.SKU_URL}${page}/${limit}`,
+    method: 'get',
+  })
+}
--- src/views/product/sku/index.vue
+++ src/views/product/sku/index.vue
@@ -1,17 +1,40 @@
 <script lang="ts" setup>
-import { ref } from "vue"
+import { ref, onMounted } from "vue"
+
+import { getSkuListAPI } from '@/api/product/sku'
+
+import type { SkuResponseData, SkuData } from "@/api/product/sku/type"
 
 let pageNo = ref(1)
 let pageSize = ref(5)
 let total = ref(0)
+let skuList = ref<SkuData[]>([])
+
+// 组件挂载完毕发请求
+onMounted(() => {
+  getSkuList()
+})
+
+const getSkuList = async (page: number = 1) => {
+  pageNo.value = page
+
+  const res: SkuResponseData = await getSkuListAPI(pageNo.value, pageSize.value)
+  if (res.code === 200) {
+    skuList.value = res.data.records
+    total.value = res.data.total
+  } else {
+    ElMessage.error(res.message)
+  }
+}
 
 // 下拉框改变每页显示条数
 const changeSize = () => {
-  // getSpuList()
+  getSkuList()
 }
 const changePageNo = (page: number) => {
-  // getSpuList(page)
+  getSkuList(page)
 }
+
 </script>
 
 <template>
@@ -20,6 +43,7 @@ const changePageNo = (page: number) => {
       <el-table
         border
         style="margin: 10px 0;"
+        :data="skuList"
       >
         <el-table-column
           label="序号"
@@ -30,25 +54,34 @@ const changePageNo = (page: number) => {
         <el-table-column
           label="名称"
           width="150px"
-          prop=""
+          prop="skuName"
         ></el-table-column>
         <el-table-column
           label="描述"
-          width="150px"
-          prop=""
+          prop="skuDesc"
+          min-width="300"
         ></el-table-column>
         <el-table-column
           label="默认图片"
           width="150px"
-          prop=""
-        ></el-table-column>
+        >
+          <template #="{ row }">
+            <el-image
+              style="width: 100%; height: 100%"
+              fit="scale-down"
+              :src="row.skuDefaultImg"
+            />
+          </template>
+        </el-table-column>
         <el-table-column
           label="重量(克)"
-          prop=""
+          prop="weight"
+          width="150px"
         ></el-table-column>
         <el-table-column
           label="价格(元)"
-          prop=""
+          prop="price"
+          width="150px"
         ></el-table-column>
         <el-table-column
           label="操作"
@@ -56,26 +89,65 @@ const changePageNo = (page: number) => {
           fixed="right"
         >
           <template #="{ row }">
-            <el-button
-              type="success"
-              icon="Top"
-            />
-            <el-button
-              type="info"
-              icon="Bottom"
-            />
-            <el-button
-              type="primary"
-              icon="Edit"
-            />
-            <el-button
-              type="InfoFilled"
-              icon="InfoFilled"
-            />
-            <el-button
-              type="danger"
-              icon="Delete"
-            />
+            <el-tooltip
+              effect="dark"
+              content="上线"
+              placement="bottom"
+            >
+              <el-button
+                type="success"
+                icon="Top"
+                size="small"
+              />
+            </el-tooltip>
+            <el-tooltip
+              effect="dark"
+              content="下线"
+              placement="bottom"
+            >
+              <el-button
+                type="info"
+                icon="Bottom"
+                size="small"
+              />
+            </el-tooltip>
+            <el-tooltip
+              effect="dark"
+              content="编辑"
+              placement="bottom"
+            >
+              <el-button
+                type="primary"
+                icon="Edit"
+                size="small"
+              />
+            </el-tooltip>
+            <el-tooltip
+              effect="dark"
+              content="信息"
+              placement="bottom"
+            >
+              <el-button
+                type="info"
+                icon="InfoFilled"
+                size="small"
+              />
+            </el-tooltip>
+            <el-popconfirm
+              :title="`确定要删除${row}`"
+              icon="DeleteFilled"
+              icon-color="#f56c6c"
+              @confirm=""
+            >
+              <template #reference>
+                <el-button
+                  type="danger"
+                  size="small"
+                  icon="Delete"
+                />
+              </template>
+            </el-popconfirm>
+
           </template>
         </el-table-column>
       </el-table>
