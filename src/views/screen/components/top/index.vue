284e892501b41f1f683b9dd7db16967a411cdf09
qiaoronggui <903111082@qq.com>
2023-08-07 20:59:05 +0800

    feat: 数据大屏 顶部搭建

---
+++ src/views/screen/components/top/index.vue
@@ -0,0 +1,109 @@
+<script lang="ts" setup>
+import { ref, onMounted, onUnmounted } from 'vue'
+import { useRouter } from 'vue-router'
+
+import moment from 'moment'
+
+const router = useRouter()
+const goHome = () => {
+  router.push('/')
+}
+// 存储当前时间
+let time = ref<any>(moment().format('YYYY年MM月DD日 HH:mm:ss'))
+let timer = ref<any>(null)
+// 开启定时器
+onMounted(() => {
+  timer.value = setInterval(() => {
+    time.value = moment().format('YYYY年MM月DD日 HH:mm:ss')
+  }, 1000)
+})
+// 组件卸载时，清除定时器
+onUnmounted(() => {
+  clearInterval(timer.value)
+})
+</script>
+
+<template>
+  <div class="top">
+    <div class="left">
+      <div
+        class="lbtn"
+        @click="goHome"
+      >首页</div>
+    </div>
+    <div class="center">
+      <div class="title">智慧旅游可视化大数据展示平台</div>
+    </div>
+    <div class="right">
+      <span class="rbtn">统计报告</span>
+      <span class="time">当前时间: {{ time }}</span>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.top {
+  display: flex;
+  width: 100%;
+  height: 100%;
+
+  .left {
+    flex: 1.5;
+    display: flex;
+    justify-content: end;
+    background: url(../../images/dataScreen-header-left-bg.png) no-repeat;
+    background-size: cover;
+
+    .lbtn {
+      width: 150px;
+      height: 40px;
+      background: url(../../images/dataScreen-header-btn-bg-l.png) no-repeat;
+      background-size: 100% 100%;
+      font-size: 20px;
+      text-align: center;
+      line-height: 40px;
+      color: #29fcff;
+      cursor: pointer;
+    }
+  }
+
+  .center {
+    flex: 2;
+
+    .title {
+      width: 100%;
+      height: 75px;
+      background: url(../../images/dataScreen-header-center-bg.png) no-repeat;
+      background-size: 100% 100%;
+      font-size: 28px;
+      text-align: center;
+      line-height: 75px;
+      color: #29fcff;
+    }
+  }
+
+  .right {
+    flex: 1.5;
+    display: flex;
+    justify-content: space-between;
+    background: url(../../images/dataScreen-header-right-bg.png) no-repeat;
+    background-size: cover;
+    text-align: center;
+    line-height: 40px;
+    color: #29fcff;
+
+    .rbtn {
+      width: 150px;
+      height: 40px;
+      background: url(../../images/dataScreen-header-btn-bg-r.png) no-repeat;
+      background-size: 100% 100%;
+      font-size: 20px;
+    }
+
+    .time {
+      font-size: 18px;
+      margin-right: 10px;
+    }
+  }
+}
+</style>
--- src/views/screen/index.vue
+++ src/views/screen/index.vue
@@ -1,7 +1,94 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { ref, onMounted, onUnmounted } from 'vue'
+
+// 引入顶部子组件
+import Top from './components/top/index.vue'
+
+// #region
+// 以下为数据大屏适配设置
+let screenRef = ref<any>(null)
+const getScale = (w = 1920, h = 1080) => {
+  const ww = window.innerWidth / w
+  const wh = window.innerHeight / h
+
+  // 返回宽高比例较小的值，保证元素能够完全显示在屏幕内
+  return ww < wh ? ww : wh
+}
+onMounted(() => {
+  screenRef.value.style.transform = `scale(${getScale()}) translate(-50%, -50%)`
+
+  // 监听窗口变化，重新计算缩放比例并设置 transform 样式
+  window.onresize = function () {
+    screenRef.value.style.transform = `scale(${getScale()}) translate(-50%, -50%)`
+  }
+})
+onUnmounted(() => {
+  // 组件卸载时，移除监听事件
+  window.onresize = null
+})
+// #endregion
+
+</script>
 
 <template>
-  <div>数据大屏</div>
+  <div class="container">
+    <!-- 数据大屏展示内容区域 -->
+    <div
+      class="screen"
+      ref="screenRef"
+    >
+      <div class="top">
+        <Top />
+      </div>
+      <div class="bottom">
+        <div class="left">left</div>
+        <div class="center">center</div>
+        <div class="right">right</div>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.container {
+  width: 100vw;
+  height: 100vh;
+  background: url(./images/bg.png) no-repeat;
+  /* background: url(./images/bg1.png) no-repeat; */
+  /* background: url(./images/bg2.png) no-repeat; */
+  /* background: url(./images/bg3.jpg) no-repeat; */
+  /* background: url(./images/bg4.jpg) no-repeat; */
+  background-size: cover;
+
+  .screen {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    width: 1920px;
+    height: 1080px;
+    transform: scale(1);
+    transform-origin: left top;
+
+    .top {
+      width: 100%;
+      height: 40px;
+    }
+
+    .bottom {
+      display: flex;
+
+      .left {
+        flex: 1;
+      }
+
+      .center {
+        flex: 2;
+      }
+
+      .right {
+        flex: 1;
+      }
+    }
+  }
+}
+</style>
